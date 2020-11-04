2e2215db64a5eaa945ba07af1f9f78cb252f1d36
gpollayil <gpollayil@gmail.com>
2020-11-04 10:06:32 +0100

    Changed color to plot accessible faces. Later, color should be given as input

--- tools/graphic/plot_box_face.m
+++ tools/graphic/plot_box_face.m
@@ -5,7 +5,8 @@ end
 
 for i = 1:length(i_face)
     face = transform_points(box.face_vertex_coordinates{i_face(i)}, box.T);
-    s = fill3(face(:,3), face(:,1), face(:,2), 'r');
+    % s = fill3(face(:,3), face(:,1), face(:,2), 'r');
+    s = fill3(face(:,3), face(:,1), face(:,2), 'c');
     alpha(s, al);
 end
 end
