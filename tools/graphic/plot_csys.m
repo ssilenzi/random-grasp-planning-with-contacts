4f10ce1380793386ee112fdf6e94d86ad1f23809
gpollayil <gpollayil@gmail.com>
2020-11-04 16:53:38 +0100

    Added handles for deleting robots from figure

--- tools/graphic/plot_csys.m
+++ tools/graphic/plot_csys.m
@@ -1,15 +1,16 @@
-function plot_csys(T, scale)
+function handle = plot_csys(T, scale)
 if nargin < 2
     scale = 1;
 end
 
 hold on
 if (size(T,1) == 4)
-    quiver3( T(3,4), T(1,4), T(2,4),  T(3,1), T(1,1), T(2,1), scale,'r')
-    quiver3( T(3,4), T(1,4), T(2,4),  T(3,2), T(1,2), T(2,2), scale,'g')
-    quiver3( T(3,4), T(1,4), T(2,4),  T(3,3), T(1,3), T(2,3), scale,'b')
+    handle(1) = quiver3( T(3,4), T(1,4), T(2,4),  T(3,1), T(1,1), T(2,1), scale,'r');
+    handle(2) = quiver3( T(3,4), T(1,4), T(2,4),  T(3,2), T(1,2), T(2,2), scale,'g');
+    handle(3) = quiver3( T(3,4), T(1,4), T(2,4),  T(3,3), T(1,3), T(2,3), scale,'b');
 elseif (size(T,1) == 3)
-    quiver(T(1,3), T(2,3), T(1,1), T(2,1), scale,'r')
-    quiver(T(1,3), T(2,3), T(1,2), T(2,2), scale,'b')
+    handle(1) = quiver(T(1,3), T(2,3), T(1,1), T(2,1), scale,'r');
+    handle(2) = quiver(T(1,3), T(2,3), T(1,2), T(2,2), scale,'b');
+    handle(3) = 0;
 end
 end
