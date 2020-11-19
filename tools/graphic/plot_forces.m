aa32309e745c6f9c12b8879fb4953b7ed0e33b41
gpollayil <gpollayil@gmail.com>
2020-11-19 15:26:02 +0100

    New and modified functions for plotting forces or points

--- tools/graphic/plot_forces.m
+++ tools/graphic/plot_forces.m
@@ -11,7 +11,7 @@ if ~exist('scale','var')
     scale = 1;
 end
 
-plot3(Cp(:,3), Cp(:,1), Cp(:,2), 'r*')
+% plot3(Cp(:,3), Cp(:,1), Cp(:,2), 'r*')
 
 quiver3(Cp(:,3), Cp(:,1), Cp(:,2), ...
     scale.*Cf(:,3), scale.*Cf(:,1), scale.*Cf(:,2), ...
