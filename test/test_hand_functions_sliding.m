420002bc2e0a8e6d7ff5840f0932ee42bafe6bec
gpollayil <gpollayil@gmail.com>
2020-11-19 15:26:24 +0100

    Now plotting also forces that do not fulfill contact constraints

--- test/test_hand_functions_sliding.m
+++ test/test_hand_functions_sliding.m
@@ -6,9 +6,9 @@ clc;
 run(fullfile('..', 'tools', 'resolve_paths.m'))
 
 % Load the environment and the box (both initial and final poses)
-run('book_on_shelf_no_target.m')
 % run('book_on_table.m')
 % run('book_on_shelf_no_other_books.m')
+run('book_on_shelf_no_target.m')
 % run('book_on_table_cluttered_no_target.m')
 axis([-10 10 -15 15 -15 15]); % Change the axis and view
 view(50, 30);
@@ -16,7 +16,7 @@ legend off;
 
 % Get contacts with the environment and plot
 [Cp, Cn] = get_contacts(environment, box_object, box_object.T);
-% plot_contacts(Cp,Cn);
+plot_contacts(Cp,Cn);
 
 % Getting the cone and moving the object
 Cone = pfc_analysis(Cp, Cn, 3);
@@ -32,12 +32,13 @@ dt = 0.5;
 %     grid off
 %     boxn = twist_moves_object(box_object, Cone(:,i)*dt);
 %     plot_boxes(all_boxes, true);
+%     plot_box(boxn.l, boxn.w, boxn.h, boxn.T, [0 0 0], true);
 %     xlabel('z');
 %     ylabel('x');
 %     zlabel('y');
 % end
 
-alpha = zeros(size(Cone,2),1); alpha(1) = 1;; % pick an alpha
+alpha = zeros(size(Cone,2),1); alpha(2) = 1; % pick an alpha
 twist = Cone*alpha*dt; % define the twist to test
 box_object2 = twist_moves_object(box_object, twist);
 plot_box(box_object2.l, box_object2.w, box_object2.h, ...
