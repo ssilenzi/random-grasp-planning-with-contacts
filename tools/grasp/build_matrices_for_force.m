06b1e1d3505a0d94b5338e95b8af70e1e553f3fb
gpollayil <gpollayil@gmail.com>
2020-11-25 14:10:54 +0100

    Modified to work in the case of no hand or no env contact

--- tools/grasp/build_matrices_for_force.m
+++ tools/grasp/build_matrices_for_force.m
@@ -16,27 +16,61 @@ function [G,J,K,H] = build_matrices_for_force(robot,Cp_h,Cn_h,Cp_e,Cn_e,Co,kh,ke
 %   G,J,K,H     - main matrices for grasp force analysis
 
 % TODO: Here maybe a dimensions check would be nice
+do_hand = [];
+if ~isempty(Cp_h) && ~isempty(Cn_h)
+    do_hand = true;
+elseif isempty(Cp_h) && isempty(Cn_h)
+    do_hand = false;
+else
+    error('You have provided only one or none between Cp_h and Cn_h');
+end
+do_env = [];
+if ~isempty(Cp_e) && ~isempty(Cn_e)
+    do_env = true;
+elseif isempty(Cp_e) && isempty(Cn_e)
+    do_env = false;
+else
+    error('You have provided only one or none between Cp_e and Cn_e');
+end
 
 % Building matrices for hand
-H_h = build_h(0,0,size(Cp_h,1),Cn_h); % hard finger
-G_h = build_g_cont(Cp_h, Co, 1);
-GHt_h = G_h * H_h.';
-J_h = robot.get_jacobian();
-HJ_h = H_h * J_h;
+if do_hand
+    H_h = build_h(0,0,size(Cp_h,1),Cn_h); % hard finger
+    G_h = build_g_cont(Cp_h, Co, 1);
+    GHt_h = G_h * H_h.';
+    J_h = robot.get_jacobian();
+    HJ_h = H_h * J_h;
+else
+    H_h = [];
+    GHt_h = [];
+    HJ_h = [];
+end
 
 % Building matrices for environment (jac is null)
-H_e = build_h(0,0,size(Cp_e,1),Cn_e); % hard finger
-G_e = build_g_cont(Cp_e, Co, 1);
-GHt_e = G_e * H_e.';
-J_e = zeros(6*size(Cp_e,1), robot.get_n_dof());
-HJ_e = H_e * J_e;
+if do_env
+    H_e = build_h(0,0,size(Cp_e,1),Cn_e); % hard finger
+    G_e = build_g_cont(Cp_e, Co, 1);
+    GHt_e = G_e * H_e.';
+    J_e = zeros(6*size(Cp_e,1), robot.get_n_dof());
+    HJ_e = H_e * J_e;
+else
+    H_e = [];
+    GHt_e = [];
+    HJ_e = [];
+end
 
 % If N_tot and D_tot are not provided, set them as identity
 if isempty(D_tot)
     D_tot = eye(size(H_e,1));
+    if ~do_env
+       D_tot = []; 
+    end
 end
 if isempty(N_tot)
     N_tot = eye(size(H_e,1));
+    if ~do_env
+       N_tot = []; 
+    end
 end
 
 % Building contact compliance matrices
