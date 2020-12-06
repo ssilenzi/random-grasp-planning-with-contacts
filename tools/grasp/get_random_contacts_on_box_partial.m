0467811963813d8530c40160059d50abd2806489
gpollayil <gpollayil@gmail.com>
2020-12-06 11:01:59 +0100

    When only hand contacts, take symmetric contacts

--- tools/grasp/get_random_contacts_on_box_partial.m
+++ tools/grasp/get_random_contacts_on_box_partial.m
@@ -30,6 +30,18 @@ for i=1:size(p,1)
     n(i,:) = box_obj.face_normals(i_face,:);
 end
 
+% If only the environment is contacting the object, then the contacts shall
+% be symmetric. THIS IS DONE ONLY IF THE NUM CONTS <= 2.
+if isempty(Cp0) && num_conts == 2 % If 0 or 1 contacts, start praying
+    p(2,:) = p(1,:); % second pos = first pos
+    n(2,:) = n(1,:); % second normal = first normal
+    ind = find(n(1,:) ~= 0); % Getting the index of the direction of cont
+
+    % Flipping the position and normal vectors in the found direction
+    p(2,ind) = -p(2,ind);
+    n(2,ind) = -n(2,ind);
+end
+
 % Transform random points to global reference system and plot
 Cp_glob = transform_points(p, box_obj.T);
 Cn_glob = transform_vectors(n, box_obj.T);
