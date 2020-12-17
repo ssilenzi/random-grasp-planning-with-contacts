87af9847b5ddc7200d0812af018a49d0111a5f10
gpollayil <gpollayil@gmail.com>
2020-12-17 16:15:25 +0100

    Added option for exhaustive search

--- tools/grasp/get_pose_from_cone.m
+++ tools/grasp/get_pose_from_cone.m
@@ -19,6 +19,7 @@ function [success,new_box_obj,twist,d_pose] = get_pose_from_cone(Cone,box_obj,en
 % would slow down everyting
 
 verbose = false;
+exhaustive_search = false;
 
 [~,cC] = size(Cone);
 
@@ -37,15 +38,19 @@ twist = Cone*alpha;
 twist = twist/norm(twist); % normalizing
 
 % Getting a collision free pose variationM and moving the box
-t_range = dt:-0.05:0;
+t_range = dt:-0.1:0;
 success = false;
 new_box_obj = box_obj;
 d_pose = twist;
+best_dt = 0;
+
+% Now we check from max dt to 0 which dt gives a collision free movement:
+% all the different dts shall be checked to avoid the issue of passing
+% though walls
 for i = 1:length(t_range)
-    % The rand is multiplied below in order to not have the same d_pose 
-    % always for the same cone (0.4*rand + 0.6) = random val in (0.6, 1)
-    d_pose = twist*t_range(i)*(0.9*rand + 0.1);
-%     d_pose = twist*t_range(i); 
+    
+    % Variation with the present dt
+    d_pose = twist*t_range(i);
     new_box_obj = twist_moves_object(box_obj, d_pose);
     
     % Checking the object env collision
@@ -54,14 +59,33 @@ for i = 1:length(t_range)
         if verbose
             fprintf('box %s collision\n', coll_type)
         end
+        best_dt = 0;
     else
         if verbose
             disp('box no collision\n');
         end
-        success = true;
-        break;
+        if exhaustive_search && t_range(i) > best_dt
+            best_dt = t_range(i);
+            continue;
+        else
+            best_dt = t_range(i);
+            break;
+        end
     end
 end
+
+if best_dt == 0
+    return;
+end
+
+% If here found a good pose
+success = true;
+
+% Building the new obj pose and d_pose with the best dt
+% The rand is multiplied below in order to not have the same d_pose 
+% always for the same cone (0.9*rand + 0.1) = random val in (0.9, 1)
+d_pose = twist*best_dt;
+new_box_obj = twist_moves_object(box_obj, d_pose);
 
 end
 
