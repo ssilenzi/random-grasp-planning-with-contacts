aa8c2f053721e1ba9a760183d178e0332778d64e
gpollayil <gpollayil@gmail.com>
2021-01-03 14:19:52 +0100

    Added collision boxes in the env files

--- scenarios/franka/franka_book_on_shelf.m
+++ scenarios/franka/franka_book_on_shelf.m
@@ -10,6 +10,8 @@ T(3,4) = 6.5*dm_to_m;
 T(2,4) = 0*dm_to_m;
 T(1,4) = 13.5*dm_to_m;
 box_object = build_box(2*dm_to_m,1*dm_to_m,3*dm_to_m,T,dm_to_m);
+box_object_col = collisionBox(2*dm_to_m,1*dm_to_m,3*dm_to_m); % coll box
+box_object_col.Pose = T;
 
 T = eye(4);
 T(3,4) = 6.5*dm_to_m;
@@ -22,27 +24,37 @@ T(3,4) = 4.75*dm_to_m;
 T(2,4) = 0.75*dm_to_m;
 T(1,4) = 13*dm_to_m;
 box_shelf = build_box(3*dm_to_m,10*dm_to_m,0.5*dm_to_m, T,dm_to_m);
+box_shelf_col = collisionBox(3*dm_to_m,10*dm_to_m,0.5*dm_to_m); % coll box
+box_shelf_col.Pose = T;
 
 T = eye(4);
 T(3,4) = 7*dm_to_m;
 T(2,4) = 3*dm_to_m;
 T(1,4) = 13.5*dm_to_m;
 box_left = build_box(2*dm_to_m,5*dm_to_m,4*dm_to_m, T,dm_to_m);
+box_left_col = collisionBox(2*dm_to_m,5*dm_to_m,4*dm_to_m); % coll box
+box_left_col.Pose = T;
 
 T = eye(4);
 T(3,4) = 7*dm_to_m;
 T(2,4) = -1.5*dm_to_m;
 T(1,4) = 13.5*dm_to_m;
 box_right = build_box(2*dm_to_m,2*dm_to_m,4*dm_to_m, T,dm_to_m);
+box_right_col = collisionBox(2*dm_to_m,2*dm_to_m,4*dm_to_m); % coll box
+box_right_col.Pose = T;
 
 T = eye(4);
 T(3,4) = 5*dm_to_m;
 T(2,4) = 1*dm_to_m;
 T(1,4) = 15*dm_to_m;
 box_wall = build_box(1*dm_to_m,10*dm_to_m,10*dm_to_m, T,dm_to_m);
+box_wall_col = collisionBox(1*dm_to_m,10*dm_to_m,10*dm_to_m); % coll box
+box_wall_col.Pose = T;
 
 environment = {box_shelf, box_left, box_right, box_wall};
 all_boxes = {box_shelf, box_left, box_right, box_wall, box_object};
+coll_boxes = {box_shelf_col, box_left_col, box_right_col, ...
+    box_wall_col, box_object_col};
 
 azim = -135.7;
 elev = 30;
--- scenarios/franka/franka_book_on_table_vertical.m
+++ scenarios/franka/franka_book_on_table_vertical.m
@@ -6,23 +6,27 @@ run(fullfile('..', 'tools', 'resolve_paths.m'))
 dm_to_m = 0.5e-1;
 
 T = eye(4);
-T(1,4) = 6*dm_to_m;
+T(1,4) = 7*dm_to_m;
 T(2,4) = 1.5*dm_to_m;
 T(3,4) = 1.75*dm_to_m;
 box_object = build_box(2*dm_to_m,1*dm_to_m,3*dm_to_m,T,dm_to_m);
+box_object_col = collisionBox(2*dm_to_m,1*dm_to_m,3*dm_to_m); % coll box
+box_object_col.Pose = T;
 
 T =  troty(pi/2);
-T(1,4) = 7.25*dm_to_m;
+T(1,4) = 7.75*dm_to_m;
 T(2,4) = 5*dm_to_m;
 T(3,4) = 4*dm_to_m;
 target_position = box_object;
 target_position.T = T;
 
 T = eye(4);
-T(1,4) = 6*dm_to_m;
+T(1,4) = 6.5*dm_to_m;
 T(2,4) = -0.25*dm_to_m;
 T(3,4) = 0;
 box_table = build_box(10*dm_to_m,10*dm_to_m,0.5*dm_to_m, T,dm_to_m);
+box_table_col = collisionBox(10*dm_to_m,10*dm_to_m,0.5*dm_to_m); % coll box
+box_table_col.Pose = T;
 
 azim = 45.7;
 elev = 50;
@@ -30,3 +34,4 @@ axis_range = [-5 5 -5 5 -1 6];
 
 environment = {box_table};
 all_boxes = {box_table, box_object};
+coll_boxes = {box_table_col, box_object_col};
