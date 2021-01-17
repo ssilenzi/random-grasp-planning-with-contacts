ffd121518d8dc968adfa16d482092de7f1a9acc1
Simone Silenzi <s.silenzi1@gmail.com>
2021-01-17 13:35:35 +0100

    Refactored names

--- books.py
+++ books.py
@@ -5,7 +5,7 @@ Description
 """
 
 import numpy as np
-import cv2
+import cv2 as cv
 
 
 def angle_cos(p0, p1, p2):
@@ -18,59 +18,61 @@ def mean(p1, p2):
 
 
 def find_squares(img):
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    squares = []
-    # for idx, gray in enumerate(cv2.split(img)):
-    for gray in cv2.split(img):
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    selectedContours = []
+    # for idx, gray in enumerate(cv.split(img)):
+    for gray in cv.split(img):
         for thrs in range(0, 255, 128):
             if thrs == 0:
-                binary = cv2.Canny(gray, 0, 50, apertureSize=5)
-                binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
+                binary = cv.Canny(gray, 0, 50, apertureSize=5)
+                binary = cv.morphologyEx(binary, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)))
             else:
-                _retval, binary = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
-            # cv2.imshow('bin_' + str(idx) + '_' + str(thrs), bin)
-            contours, _hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                _retval, binary = cv.threshold(gray, thrs, 255, cv.THRESH_BINARY)
+            # cv.imshow('bin_' + str(idx) + '_' + str(thrs), bin)
+            contours, _hierarchy = cv.findContours(binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
             for cnt in contours:
-                cnt_len = cv2.arcLength(cnt, True)
-                cnt = cv2.approxPolyDP(cnt, 0.005 * cnt_len, True)
+                cnt_len = cv.arcLength(cnt, True)
+                cnt = cv.approxPolyDP(cnt, 0.005 * cnt_len, True)
                 if any(min(point) < 2 or
                        point[0] > (img.shape[1] - 2) or
                        point[1] > (img.shape[0] - 2) for point in cnt[0]):
                     continue
-                if len(cnt) >= 4 and cv2.contourArea(cnt) > 1000:  # and cv2.isContourConvex(cnt):
+                if len(cnt) >= 4 and cv.contourArea(cnt) > 1000:  # and cv.isContourConvex(cnt):
                     cnt = cnt.reshape(-1, 2)
                     max_cos = np.max([angle_cos(cnt[i],
                                                 cnt[(i + 1) % len(cnt)],
                                                 cnt[(i + 2) % len(cnt)]) for i in range(len(cnt))])
                     if max_cos < 0.4:
-                        squares.append(cnt)
-    return squares
+                        selectedContours.append(cnt)
+    return selectedContours
 
 
 def main():
     from glob import glob
-    for fn in glob('source/library2.jpg'):
-        img = cv2.imread(fn)
+    for fn in glob('source/library.jpg'):
+        imgTop = cv.imread(fn)
+        imgFront = cv.imread(fn)
+        imgTop2 = imgTop.copy()
+        imgFront2 = imgFront.copy()
 
-        img2 = img.copy()
+        selectedContours = find_squares(imgTop)
+        cv.drawContours(imgTop, selectedContours, -1, color=(0, 0, 255), thickness=1)
+        contourTop = selectedContours[0]
+        cv.imshow('Top profile', imgTop)
+        # cv.imshow('Front profile', imgFront)
 
-        squares = find_squares(img)
-        cv2.drawContours(img, squares, -1, color=(0, 0, 255), thickness=1)
+        nBoxes = int(len(contourTop) / 4)
+        box = np.empty([nBoxes, 2, 2], dtype=int)
+        for i in range(nBoxes):
+            box[i] = [[mean(contourTop[1 + 2 * i, 0], contourTop[-2 - 2 * i, 0]),
+                       mean(contourTop[2 * i, 1], contourTop[1 + 2 * i, 1])],
+                      [mean(contourTop[2 * i, 0], contourTop[-1 - 2 * i, 0]),
+                       mean(contourTop[-2 - 2 * i, 1], contourTop[-1 - 2 * i, 1])]]
+            cv.rectangle(imgTop2, tuple(box[i, 0]), tuple(box[i, 1]), color=(255, 0, 0), thickness=1)
+        cv.imshow("Top boxes", imgTop2)
+        cv.imwrite("dest/top_boxes.jpg", imgTop2)
 
-        approx = squares[0]
-        n_boxes = int(len(approx) / 4)
-        box = np.empty([n_boxes, 2, 2], dtype=int)
-        for i in range(n_boxes):
-            box[i] = [[mean(approx[1 + 2 * i, 0], approx[-2 - 2 * i, 0]),
-                       mean(approx[2 * i, 1], approx[1 + 2 * i, 1])],
-                      [mean(approx[2 * i, 0], approx[-1 - 2 * i, 0]),
-                       mean(approx[-2 - 2 * i, 1], approx[-1 - 2 * i, 1])]]
-            cv2.rectangle(img2, tuple(box[i, 0]), tuple(box[i, 1]), color=(255, 0, 0), thickness=1)
-        cv2.imshow("rectangles", img2)
-        cv2.imwrite("dest/rectangles.jpg", img2)
-
-        cv2.imshow('squares', img)
-        ch = cv2.waitKey()
+        ch = cv.waitKey()
         if ch == 27:
             break
 
@@ -78,4 +80,4 @@ def main():
 if __name__ == '__main__':
     print(__doc__)
     main()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
--- camera.py
+++ camera.py
@@ -4,23 +4,23 @@
 Description
 """
 
-import cv2
+import cv2 as cv
 
 
 def main():
     from sys import platform
     if platform == "win32":
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        cap = cv.VideoCapture(0, cv.CAP_DSHOW)
     else:
-        cap = cv2.VideoCapture(1)
+        cap = cv.VideoCapture(1)
     # Check if the webcam is opened correctly
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
     while True:
         ret, frame = cap.read()
-        cv2.imshow('Input', frame)
+        cv.imshow('Input', frame)
 
-        c = cv2.waitKey(1)
+        c = cv.waitKey(1)
         if c == 27:
             break
     cap.release()
@@ -29,4 +29,4 @@ def main():
 if __name__ == '__main__':
     print(__doc__)
     main()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
