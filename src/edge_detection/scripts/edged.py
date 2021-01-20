35ffbd82283b26c909eb3d97134c3b0c3eb94782
Simone Silenzi <s.silenzi1@gmail.com>
2021-01-20 20:37:23 +0100

    Minor fixes

--- src/edge_detection/scripts/edged.py
+++ src/edge_detection/scripts/edged.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-It detects boxes
+Edge detection package
+by S. Silenzi version 0.1.0
 """
 
 import rospy
@@ -10,43 +11,46 @@ import cv2 as cv
 import numpy as np
 
 
+# init camera function
+def initCameras(cams, caps):
+    for cam in cams:
+        # noinspection PyArgumentList
+        cap = cv.VideoCapture(cam)
+        if not cap.isOpened():
+            for ocap in caps:
+                ocap.release()
+            raise IOError('Cannot open camera ' + str(cam))
+        caps.append(cap)
+
+
 # node main program
-def edged():
+def main():
     print(__doc__)
     rospy.init_node('edge_detector')
-    # init first camera
-    cam1 = 2
-    # noinspection PyArgumentList
-    cap1 = cv.VideoCapture(cam1)
-    if not cap1.isOpened():
-        raise IOError('Cannot open camera ' + str(cam1))
-    # init second camera
-    cam2 = 0
-    # noinspection PyArgumentList
-    cap2 = cv.VideoCapture(cam2)
-    if not cap2.isOpened():
-        cap1.release()
-        raise IOError('Cannot open camera ' + str(cam2))
+    # init cameras
+    cams = [2, 0]
+    caps = []
+    initCameras(cams, caps)
     # main loop
     rate = rospy.Rate(10)  # Hz
     while not rospy.is_shutdown():
-        ret, frame1 = cap1.read()
-        ret, frame2 = cap2.read()
-        cv.imshow('Camera ' + str(cam1), frame1)
-        cv.imshow('Camera ' + str(cam2), frame2)
+        ret, frame1 = caps[0].read()
+        ret, frame2 = caps[1].read()
+        cv.imshow('Camera ' + str(cams[0]), frame1)
+        cv.imshow('Camera ' + str(cams[1]), frame2)
         c = cv.waitKey(1)
         if c != 255:
             break
         rospy.loginfo("Showed images")
         rate.sleep()
     # close program
-    cap1.release()
-    cap2.release()
+    caps[0].release()
+    caps[1].release()
     cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
     try:
-        edged()
+        main()
     except rospy.ROSInterruptException:
         pass
