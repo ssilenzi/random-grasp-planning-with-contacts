02b8275d31993c612261a0b92c9032fd87a4b569
Simone Silenzi <s.silenzi1@gmail.com>
2021-01-21 02:09:03 +0100

    buildBoxes

--- src/edge_detection/scripts/edged.py
+++ src/edge_detection/scripts/edged.py
@@ -11,21 +11,42 @@ import cv2 as cv
 import numpy as np
 
 
+def mean(p1, p2):
+    if isinstance(p1, int) and isinstance(p2, int):
+        meanValue = int((p1 + p2) / 2)
+    else:
+        meanValue = (p1 + p2) / 2
+    return meanValue
+
+
+def buildBoxes(boxesIso):
+    boxes = []
+    for idx, boxiso in enumerate(boxesIso):
+        dbox = tuple((boxiso[1] - boxiso[0]).tolist())
+        # rotmatrix = np.identity(3)
+        rotmatrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        rotmatrix = tuple((rotmatrix.reshape(9, 1)).tolist())
+        center = mean(boxiso[0], boxiso[1])
+        box = Box(idx=idx, width=dbox[0], length=dbox[1], height=dbox[2], rotmatrix=rotmatrix, center=center)
+        boxes.append(box)
+    return boxes
+
+
 # init camera function
 def initCameras(cams, caps):
     for cam in cams:
         # noinspection PyArgumentList
         cap = cv.VideoCapture(cam)
         if not cap.isOpened():
-            for ocap in caps:
-                ocap.release()
+            for othercap in caps:
+                othercap.release()
             raise IOError('Cannot open camera ' + str(cam))
         caps.append(cap)
 
 
 def main():
     print(__doc__)
-    rospy.init_node('edge_detector')
+    rospy.init_node('edged')
     # init cameras
     cams = [2, 0]
     caps = []
@@ -41,9 +62,9 @@ def main():
                 break
             frames.append(frame)
         if ret:
-            for i, cam in enumerate(cams):
-                cv.imshow('Camera ' + str(cam), frames[i])
-            rospy.loginfo("Printed images")
+            for itr, cam in enumerate(cams):
+                cv.imshow('Camera ' + str(cam), frames[itr])
+            rospy.loginfo('Printed images')
         c = cv.waitKey(1)
         if c != 255:
             break
