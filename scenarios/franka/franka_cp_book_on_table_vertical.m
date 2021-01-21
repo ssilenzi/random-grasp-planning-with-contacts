dcf7f6eb372dd30db3a0df15bccaa5539bdd2e98
gpollayil <gpollayil@gmail.com>
2021-01-21 16:31:06 +0100

    Minor changes

--- scenarios/franka/franka_cp_book_on_table_vertical.m
+++ scenarios/franka/franka_cp_book_on_table_vertical.m
@@ -8,20 +8,20 @@ dm_to_m = 1e-1;
 T = eye(4);
 T(1,4) = 5.5*dm_to_m;
 T(2,4) = 0*dm_to_m;
-T(3,4) = 1.27*dm_to_m;
+T(3,4) = 1.275*dm_to_m;
 box_object = build_box(1.3*dm_to_m,0.55*dm_to_m,2.55*dm_to_m,T);
 
 T =  trotz(-pi/2);
-T(1,4) = 5*dm_to_m;
+T(1,4) = 6.5*dm_to_m;
 T(2,4) = 2.75*dm_to_m;
-T(3,4) = 1.27*dm_to_m;
+T(3,4) = 1.275*dm_to_m;
 target_position = box_object;
 target_position.T = T;
 
 T = eye(4);
-T(1,4) = 3.25*dm_to_m;
+T(1,4) = 6*dm_to_m;
 T(2,4) = 0*dm_to_m;
-T(3,4) = -0.13*dm_to_m;
+T(3,4) = -0.125*dm_to_m;
 box_table = build_box(10*dm_to_m,7*dm_to_m,0.25*dm_to_m, T);
 
 azim = 75.5;
