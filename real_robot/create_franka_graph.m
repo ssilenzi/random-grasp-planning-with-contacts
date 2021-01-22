8cac3cd7a6e696caca1588fd29766d1cce2a29d1
gpollayil <gpollayil@gmail.com>
2021-01-22 17:24:33 +0100

    Now getting the poses for graph

--- real_robot/create_franka_graph.m
+++ real_robot/create_franka_graph.m
@@ -1,6 +1,6 @@
 function [G_out, ind_sol, nearest] = ...
-    create_franka_graph(G, env, obj_fin, n_expand, tol,...
-    edge_types, edge_weights);
+    create_franka_graph(G, env, obj_fin, n_expand,...
+    edge_types, edge_weights)
 
 rosshutdown
 rosinit('172.16.0.6');
@@ -32,16 +32,53 @@ for i = 1:n_expand
         ~, ~, ~] = get_node_properties(node_s);
     
     % Creating the next robot
-    franka_m = copy(franka_s);
-    franka_m.set_config(q_arr);
-    franka_m.plot([], false, gca);
-    plot3(Cp(:,1), Cp(:,2), Cp(:,3), 'r*')
+    franka_f = copy(franka_s);
+    franka_f.set_config(q_arr);
+    franka_f.plot([], false, gca);
+%     plot3(Cp(:,1), Cp(:,2), Cp(:,3), 'r*')
     
-    % Projecting the contacts on the nearest faces of box
-    box_m = copy(box_s);
+    % What is this transition? 1 pos, 2 mov, 3 rel
+    prompt = 'What is this transition? ';
+    tr = [];
+    while true
+        tr = input(prompt); 
+        if (tr == 1 || tr == 2 || tr == 3)
+            break;
+        end
+    end
     
+    % If moving, rotating the box according to the franka wrist rotation
+    box_f = box_s;
+    if (tr == 3) % moving
+        T1 = franka_s.T_all(:,:,9);
+        T2 = franka_f.T_all(:,:,9);
+        T21 = T2*inv(T1);
+        box_f.T = T21*box_f.T;
+    end
+    
+    % Other values
+    Cont_h_f = true;
+    if (tr == 3) 
+        Cont_h_f = false;
+    end
+    dist_f = hom_dist(box_f.T, obj_fin.T);
+    
+    % Creating the moved node
+    node_f = create_node(n_nodes+1, box_f, franka_f, [], [], [], ...
+        Cont_h_f, [], [], [], dist_f, []);
+    
+    % Creating weighted edge between nodes
+    e_type_sf = edge_types{tr};
+    e_weight_sf = edge_weights(tr);
+    edge_f = table({e_type_sf}', e_weight_sf, ...
+        'VariableNames',{'Type','Weight'});
+    
+    % Adding the newly created nodes and edges
+  	G = add_nodes_edges_to_graph(G,node_f,edge_f,n_nodes);
     
 end
+
+G_out = G;
 
 end
 
